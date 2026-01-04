e13f4d200dc132521f59b57d01c108817c1e6a34
Boris Truong <linhtch90@gmail.com>
2026-01-05 01:04:55 +0700

    [Leetcode-103] - Binary tree zigzag level order traversal

---
+++ main/Leetcode-103-Binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,62 @@
+using namespace std;
+
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+    {
+        vector<vector<int>> result;
+
+        if (root == nullptr)
+        {
+            return result;
+        }
+
+        queue<TreeNode *> nodeQueue;
+        nodeQueue.push(root);
+        bool isLeftToRight = true;
+
+        while (!nodeQueue.empty())
+        {
+            int queueSize = nodeQueue.size();
+            vector<int> currentLevel;
+            for (int i = 0; i < queueSize; i++)
+            {
+                TreeNode *currentNode = nodeQueue.front();
+                nodeQueue.pop();
+                currentLevel.push_back(currentNode->val);
+                if (currentNode->left != nullptr)
+                {
+                    nodeQueue.push(currentNode->left);
+                }
+                if (currentNode->right != nullptr)
+                {
+                    nodeQueue.push(currentNode->right);
+                }
+            }
+            if (!isLeftToRight)
+            {
+                reverse(currentLevel.begin(), currentLevel.end());
+            }
+            result.push_back(currentLevel);
+            isLeftToRight = !isLeftToRight;
+        }
+
+        return result;
+    }
+};
