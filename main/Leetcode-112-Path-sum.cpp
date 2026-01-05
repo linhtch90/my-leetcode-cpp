611f08e069ba998a24e84ea7fa1a05b2b3896c51
Boris Truong <linhtch90@gmail.com>
2026-01-05 22:05:02 +0700

    [Leetcode-112] - Path sum

---
+++ main/Leetcode-112-Path-sum.cpp
@@ -0,0 +1,37 @@
+//
+// Created by boris on 1/5/26.
+//
+
+using namespace std;
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
+    }
+};
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode *root, int targetSum) {
+        if (root == nullptr) {
+            return false;
+        }
+
+        if (root->val == targetSum && root->left == nullptr && root->right == nullptr) {
+            return true;
+        }
+
+        return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+    }
+};
