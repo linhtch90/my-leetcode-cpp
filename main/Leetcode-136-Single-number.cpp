11512f5bbcef0c1535da8ca3422446fb74496b7a
Boris Truong <linhtch90@gmail.com>
2026-01-14 01:18:06 +0700

    [Leetcode-136] - Single number

---
+++ main/Leetcode-136-Single-number.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+
+#include <vector>
+
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int result = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            result ^= nums[i];
+        }
+        return result;
+    }
+};
